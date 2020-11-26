de0cdc9eeeec767cd7cfd17fd0968494eb8ca394
andyjmaclean <andyjmaclean@gmail.com>
2020-11-26 11:13:37 +0100

    Add models / test travis

---
+++ src/app/_models/models.ts
@@ -0,0 +1,42 @@
+export type HeaderNameType = 'name' | 'count' | 'percent';
+
+export interface MenuState {
+  visible: boolean;
+  disabled?: boolean;
+}
+
+export interface FacetField {
+  count: number;
+  label: HeaderNameType;
+}
+
+export interface TableRow {
+  name: HeaderNameType;
+  count: string;
+  percent: string;
+}
+
+export interface FmtTableData {
+  columns: Array<string>;
+  tableRows: Array<TableRow>;
+}
+
+export interface Facet {
+  name: string;
+  fields: Array<FacetField>;
+}
+
+export interface NameValue {
+  name: string;
+  value: number;
+}
+
+export interface RawFacet {
+  facets: Array<Facet>;
+  totalResults: number;
+}
+
+export enum ExportType {
+  CSV = 'CSV',
+  PDF = 'PDF',
+}
