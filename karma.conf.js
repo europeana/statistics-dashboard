cc613f6dba2b3c3c4e04ad587caa18413003f039
andyjmaclean <andyjmaclean@gmail.com>
2020-12-21 15:10:19 +0100

    Karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -6,21 +6,23 @@ module.exports = function (config) {
     basePath: "",
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
+      require("karma-coverage"),
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
       require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/statistics-dashboard"),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
     },
-    reporters: ["progress", "kjhtml"],
+    preprocessors: {
+      'src/app/**/*.ts': ['coverage']
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
