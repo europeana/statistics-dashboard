aeb0768b54e6de5821e17a1bb8f44a8c1392ad89
andyjmaclean <andyjmaclean@gmail.com>
2021-09-16 16:24:21 +0200

    Use statistics dashboard API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
-
-  serverAPI: 'https://api.europeana.eu/record/v2/search.json',
+  serverAPI: 'https://statistics-dashboard-rest-test.eanadev.org/',
   serverPortal: 'https://www.europeana.eu/en/search',
-  useDataServer: false,
+  useDataServer: true,
   intervalStatus: 2500,
   intervalStatusMax: 60000 * 9.5,
   wskey: 'api2demo'
