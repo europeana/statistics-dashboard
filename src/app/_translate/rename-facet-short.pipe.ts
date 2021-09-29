753c0457c77dac3764352111506b6f8333a5bede
andyjmaclean <andyjmaclean@gmail.com>
2021-09-29 16:35:25 +0200

    Use dimensionName variable in translators

--- src/app/_translate/rename-facet-short.pipe.ts
+++ src/app/_translate/rename-facet-short.pipe.ts
@@ -4,24 +4,25 @@
 /* supplies human-readable labels for facet names
 */
 import { Pipe, PipeTransform } from '@angular/core';
+import { DimensionName } from '../_models';
 
-const facetNames: { [key: string]: string } = {
-  contentTier: 'Tier',
-  metadataTier: 'Tier',
-  country: 'Country',
-  type: 'Type',
-  rights: 'Rights',
-  dataProvider: 'CHI',
-  provider: 'Provider',
+const facetNames: { [key: string]: string } = {};
 
-  plural_contentTier: 'Tiers',
-  plural_metadataTier: 'Tiers',
-  plural_country: 'Countries',
-  plural_type: 'Types',
-  plural_rights: 'Rights',
-  plural_dataProvider: 'CHIs',
-  plural_provider: 'Providers'
-};
+facetNames[DimensionName.contentTier] = 'Tier';
+facetNames[DimensionName.metadataTier] = 'Tier';
+facetNames[DimensionName.country] = 'Country';
+facetNames[DimensionName.type] = 'Type';
+facetNames[DimensionName.rights] = 'Rights';
+facetNames[DimensionName.dataProvider] = 'CHI';
+facetNames[DimensionName.provider] = 'Provider';
+
+facetNames[`plural_${DimensionName.contentTier}`] = 'Tiers';
+facetNames[`plural_${DimensionName.metadataTier}`] = 'Tiers';
+facetNames[`plural_${DimensionName.country}`] = 'Countries';
+facetNames[`plural_${DimensionName.type}`] = 'Types';
+facetNames[`plural_${DimensionName.rights}`] = 'Rights';
+facetNames[`plural_${DimensionName.dataProvider}`] = 'CHIs';
+facetNames[`plural_${DimensionName.provider}`] = 'Providers';
 
 @Pipe({
   name: 'renameApiFacetShort'
--- src/app/_translate/rename-facet.pipe.ts
+++ src/app/_translate/rename-facet.pipe.ts
@@ -4,16 +4,17 @@
 /* supplies human-readable labels for facet names
 */
 import { Pipe, PipeTransform } from '@angular/core';
+import { DimensionName } from '../_models';
 
-const facetNames: { [key: string]: string } = {
-  contentTier: 'Content Tier',
-  metadataTier: 'Metadata Tier',
-  country: 'Country',
-  type: 'Type',
-  rights: 'Rights',
-  dataProvider: 'Data Provider',
-  provider: 'Provider'
-};
+const facetNames: { [key: string]: string } = {};
+
+facetNames[DimensionName.contentTier] = 'Content Tier';
+facetNames[DimensionName.metadataTier] = 'Metadata Tier';
+facetNames[DimensionName.country] = 'Country';
+facetNames[DimensionName.type] = 'Type';
+facetNames[DimensionName.rights] = 'Rights';
+facetNames[DimensionName.dataProvider] = 'Data Provider';
+facetNames[DimensionName.provider] = 'Provider';
 
 @Pipe({
   name: 'renameApiFacet'
