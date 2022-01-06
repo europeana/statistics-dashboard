e441a1225c2ff01b5adc1f7cd14f5e4ff1916da6
andyjmaclean <andyjmaclean@gmail.com>
2022-01-06 16:15:48 +0100

    Make files match

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: true,
-  serverAPI: 'https://statistics-dashboard-rest-test.eanadev.org/',
-  serverPortal: 'https://www.europeana.eu/en/search',
   intervalStatus: 2500,
   intervalStatusMax: 60000 * 9.5,
+  production: true,
+  serverAPI: 'https://metis-statistics-rest-test.eanadev.org/',
+  serverPortal: 'https://www.europeana.eu/en/search',
   wskey: 'api2demo'
 };
