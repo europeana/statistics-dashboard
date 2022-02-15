2618dbd7be3527a4d60d054452cee11662c1be79
andyjmaclean <andyjmaclean@gmail.com>
2022-02-15 15:17:51 +0100

    Footer links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   production: false,
   //serverAPI: 'https://api.europeana.eu/record/v2/search.json',
   serverAPI: 'https://metis-statistics-rest-test.eanadev.org/',
-  serverPortal: 'https://www.europeana.eu/en/search',
+  serverPortal: 'https://www.europeana.eu/search',
   wskey: 'api2demo'
 };
