4196d8910ff1ac895c42c3d6d9f29b8e812a549c
andyjmaclean <andyjmaclean@gmail.com>
2022-08-17 16:55:26 +0200

    Tidy

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,8 +11,6 @@ export default defineConfig({
     dataServer: 'http://127.0.0.1:3001',
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
