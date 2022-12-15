42e2f5b48009c2344040967529d2e9e53c1ebf00
andyjmaclean <andyjmaclean@gmail.com>
2022-12-15 16:31:46 +0100

    MET-5007 Move env to assets / js preload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,14 @@
 /*  settings that can be used throughout the entire application
  **/
 
+const getEnvVar = (key: string): string => {
+  return (window as any).__env[key];
+};
+
 export const environment = {
-  feedbackUrl: 'https://europeana.atlassian.net/servicedesk/customer/portal/10',
   production: false,
-  //serverAPI: 'https://api.europeana.eu/record/v2/search.json',
-  serverAPI: 'https://metis-statistics-rest-test.eanadev.org/',
-  serverPortal: 'https://www.europeana.eu/search',
-  wskey: 'api2demo'
+  feedbackUrl: getEnvVar('feedbackUrl') || '',
+  serverAPI: getEnvVar('serverAPI') || '',
+  serverPortal: getEnvVar('serverPortal') || '',
+  wskey: getEnvVar('wskey') || ''
 };
