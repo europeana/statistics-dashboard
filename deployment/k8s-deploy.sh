2b043fcd28adde516b13cf0d9a8fb4b6dedd8301
andyjmaclean <andyjmaclean@gmail.com>
2023-01-20 17:07:05 +0100

    Make context configurable from command line / change flag names to c, d, t

--- deployment/k8s-deploy.sh
+++ deployment/k8s-deploy.sh
@@ -2,35 +2,32 @@
 export IMAGE_TAG=v3.0
 export IMAGE_ORGANISATION=andyjmaclean
 export IMAGE_NAME=statistics-dashboard-app-image
-# TODO override with command line
+
+# Default context / delete / target
 export CONTEXT=minikube
+export DELETE=false
 export TARGET=local
 
-# Utility to read command line arguments
-has_param() {
-  local term="$1"
-  shift
-  for arg; do
-    if [[ $arg == "$term" ]]; then
-      return 0
-    fi
-  done
-  return 1
-}
-
-if has_param '-target=test' "$@"; then
-  TARGET=test
-elif has_param '-target=acceptance' "$@"; then
-  TARGET=acceptance
-elif has_param '-target=production' "$@"; then
-  TARGET=production
-  echo "in production"
-elif ! has_param '-target=test' "$@"; then
-  echo "in local"
-fi
+# Override default context / delete / target
+while getopts ":c:dt:" o; do
+  case "${o}" in
+    c)
+      CONTEXT=${OPTARG}
+      ;;
+    d)
+      DELETE=true
+      ;;
+    t)
+      TARGET=${OPTARG}
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+echo "CONTEXT = ${CONTEXT}, DELETE = ${DELETE}, TARGET = ${TARGET}"
 
 # If deleting then delete and exit
-if has_param '--delete' "$@"; then
+if $DELETE;
+then
   kubectl --context $CONTEXT delete -k deployment/$TARGET/
   exit 0;
 fi
