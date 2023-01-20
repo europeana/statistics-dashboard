fe01bb28b7f58dc5aa6a6ef85d145d06386e161f
andyjmaclean <andyjmaclean@gmail.com>
2023-01-20 15:34:00 +0100

    Parameterise deployment target

--- deployment/k8s-deploy.sh
+++ deployment/k8s-deploy.sh
@@ -4,20 +4,34 @@ export IMAGE_ORGANISATION=andyjmaclean
 export IMAGE_NAME=statistics-dashboard-app-image
 # TODO override with command line
 export CONTEXT=minikube
+export TARGET=local
 
-if echo $* | grep -e "--delete" -q
-then
-  kubectl --context $CONTEXT delete -k deployment/local/
+has_param() {
+  local term="$1"
+  shift
+  for arg; do
+    if [[ $arg == "$term" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+if has_param '-target=test' "$@"; then
+  TARGET=test
+  echo "in test"
+elif ! has_param '-target=test' "$@"; then
+  echo "in local"
+fi
+if has_param '--delete' "$@"; then
+  kubectl --context $CONTEXT delete -k deployment/$TARGET/
   exit 0;
 fi
 
 # Update deployment.yaml with IMAGE variables
-sed -i "s/IMAGE_TAG/$IMAGE_TAG/g" deployment/local/deployment.yaml
-sed -i "s/IMAGE_ORGANISATION/$IMAGE_ORGANISATION/g" deployment/local/deployment.yaml
-sed -i "s/IMAGE_NAME/$IMAGE_NAME/g" deployment/local/deployment.yaml
+sed -i "s/IMAGE_TAG/$IMAGE_TAG/g" deployment/$TARGET/deployment.yaml
+sed -i "s/IMAGE_ORGANISATION/$IMAGE_ORGANISATION/g" deployment/$TARGET/deployment.yaml
+sed -i "s/IMAGE_NAME/$IMAGE_NAME/g" deployment/$TARGET/deployment.yaml
 
-kubectl --context $CONTEXT apply -k deployment/local/
-
-# TODO parameterise namespace
-#   by changing in kustomize.yaml files under test / acceptance / production 
-git checkout deployment/local/deployment.yaml
+kubectl --context $CONTEXT apply -k deployment/$TARGET/
+git checkout deployment/$TARGET/deployment.yaml
