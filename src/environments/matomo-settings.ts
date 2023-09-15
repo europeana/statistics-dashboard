a358f3af8caa3d3bf985d61e85ccad21213bd629
andyjmaclean <andyjmaclean@gmail.com>
2023-09-15 12:27:58 +0200

    MET-5441 Add Matomo Tracking

---
+++ src/environments/matomo-settings.ts
@@ -0,0 +1,16 @@
+import { MaintenanceScheduleItemKey } from '@europeana/metis-ui-maintenance-utils';
+
+const getEnvVar = (key: string): string | null => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const env = (window as any).__env;
+  return env ? env[key] : null;
+};
+
+const matomoHost = getEnvVar('matomoHost');
+const matomoSiteId = getEnvVar('matomoSiteId');
+
+export const matomoSettings = {
+  matomoHost,
+  matomoScriptUrl: `${matomoHost}/matomo.js`,
+  matomoSiteId: parseInt(matomoSiteId)
+};
