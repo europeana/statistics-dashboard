18123d73101deda21ea76c036faa8d20d46069f7
andyjmaclean <andyjmaclean@gmail.com>
2023-09-18 09:51:51 +0200

    Remove unused import

--- src/environments/matomo-settings.ts
+++ src/environments/matomo-settings.ts
@@ -1,5 +1,3 @@
-import { MaintenanceScheduleItemKey } from '@europeana/metis-ui-maintenance-utils';
-
 const getEnvVar = (key: string): string | null => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const env = (window as any).__env;
