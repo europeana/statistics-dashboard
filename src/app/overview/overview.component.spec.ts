80296fccf5100e60d4c66d31dd66bef16b1137b8
andyjmaclean <andyjmaclean@gmail.com>
2024-03-27 12:12:30 +0100

    MET-5843 Github Actions Deployment

--- src/app/overview/overview.component.spec.ts
+++ src/app/overview/overview.component.spec.ts
@@ -47,7 +47,7 @@ import { GridComponent } from '../grid';
 import { SnapshotsComponent } from '../snapshots';
 import { OverviewComponent } from './overview.component';
 
-describe('OverviewComponent', () => {
+fdescribe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
   let router: Router;
@@ -156,6 +156,10 @@ describe('OverviewComponent', () => {
     beforeEach(() => {
       b4Each();
       params.next({ facet: DimensionName.country });
+    });
+
+    it('should intentionally fail', () => {
+      expect(false).toBeTruthy();
     });
 
     it('should load on initialisation', fakeAsync(() => {
