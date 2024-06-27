8c3ffffd45ccdc0f4f7397905db1a08af3800677
andyjmaclean <andyjmaclean@gmail.com>
2024-06-27 09:04:09 +0200

    MET-6011 Use Matomo Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
 import { LandingComponent } from './landing/landing.component';
+import { CountryComponent } from './country/country.component';
+import {
+  MatomoRouterModule,
+  MatomoRouteDataInterceptor
+} from 'ngx-matomo-client';
 
+const pageTitle = 'Statistics Dashboard';
 const routes: Routes = [
   {
+    title: `${pageTitle} | Filters`,
     path: 'data/:facet',
     component: OverviewComponent
   },
   {
     path: 'cookie-policy',
+    title: `${pageTitle} | Cookie Policy`,
     loadComponent: async () =>
       (await import('./cookie-policy/cookie-policy.component'))
         .CookiePolicyComponent
   },
   {
     path: 'privacy-policy',
+    title: `${pageTitle} | Privacy Policy`,
     loadComponent: async () =>
       (await import('./privacy-policy/privacy-policy.component'))
         .PrivacyPolicyComponent
   },
   {
+    title: pageTitle,
     path: '',
     component: LandingComponent
   },
@@ -28,7 +38,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    MatomoRouterModule.forRoot({
+      // Declare built-in MatomoRouteDataInterceptor
+      interceptors: [MatomoRouteDataInterceptor]
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
