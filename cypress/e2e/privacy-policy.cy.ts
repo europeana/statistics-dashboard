060e68fa4febfedb5656b9c6843cdcd4c0939042
andyjmaclean <andyjmaclean@gmail.com>
2024-07-10 16:39:41 +0200

    MET-6016 Rename Privacy Policy

--- cypress/e2e/privacy-policy.cy.ts
+++ cypress/e2e/privacy-policy.cy.ts
@@ -1,21 +1,21 @@
 context('Statistics Dashboard', () => {
-  describe('privacy policy', () => {
+  describe('privacy statement', () => {
     beforeEach(() => {
       cy.visit('/');
     });
 
-    const selPrivacy = '[data-e2e="privacy-policy"]';
+    const selPrivacy = '[data-e2e="privacy-statement"]';
 
-    it('should show the privacy policy when the user goes there directly', () => {
+    it('should show the privacy statement when the user goes there directly', () => {
       cy.get(selPrivacy).should('not.exist');
       cy.visit('/privacy-policy');
       cy.get(selPrivacy).should('have.length', 1);
     });
 
-    it('should show the privacy policy when the user clicks the footer link', () => {
+    it('should show the privacy statement when the user clicks the footer link', () => {
       cy.get(selPrivacy).should('not.exist');
       cy.get('a')
-        .contains('Privacy Policy')
+        .contains('Privacy Statement')
         .should('have.length', 1)
         .click({ force: true });
       cy.get(selPrivacy).should('have.length', 1);
