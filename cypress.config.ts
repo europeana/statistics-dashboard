d2f28fe2c5c5631fc34cabc643b99b70cb11931a
andyjmaclean <andyjmaclean@gmail.com>
2026-03-30 21:06:48 +0200

    MET-7066 Node Update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'http://localhost:4280',
   },
 });
